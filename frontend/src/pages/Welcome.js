ec50d7c6aacf5c1da6da0939082693cfb92bae0c
cassidychu <cassidychu@yahoo.com>
2023-05-07 21:49:04 -0700

    php added and changed welcome page

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -1,46 +1,67 @@
 import React from "react";
-import { Text, Flex, Box, Button, Center, Link } from "@chakra-ui/react";
+import {
+  Text,
+  Flex,
+  Box,
+  Button,
+  Center,
+  Link,
+  HStack,
+  VStack,
+  Heading,
+} from "@chakra-ui/react";
 
-// const handleSubmit = async (e) => {
-//   e.preventDefault();
-// };
 export default function Welcome() {
   return (
-    <div>
-      <Flex
-        w="100vw"
-        h="100vh"
-        backgroundColor="#3c3f63"
-        display="flex"
-        align="center"
-        justify="center"
-        direction="column"
-      >
-        <Box color="#d87e79" fontWeight="extrabold" fontSize="6xl" mt={40}>
-          Welcome to Little Chef
-        </Box>
-        <Box paddingTop={10}>
-          <Link href="/home">
-            <Button
-              color="#d87e79"
-              size="lg"
-              height="70px"
-              width="600px"
-              type="submit"
-              onClick={() => {}}
-            >
-              Get Started
-            </Button>
-          </Link>
-        </Box>
-      </Flex>
-    </div>
-
-    // <div className="home">
-    //   <h1>Welcome to Little Chef</h1>
-    //   <button type="submit" className="GetStartedButton" onClick={handleSubmit}>
-    //     Get Started <a href="./welcome"></a>
-    //   </button>
-    // </div>
+    <Box backgroundColor="#3c3f63" w={"100vw"} h={"100vh"}>
+      <Center pt="25%">
+        <Flex
+          my={10}
+          w={"100%"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap={8}
+        >
+          <VStack
+            w={["95%", "80%", "60%", "50%", "35%"]}
+            h={"100%"}
+            p={10}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={2}
+          >
+            <Heading fontSize="6xl" mt={40} textColor="#d87e79">
+              Welcome to Little Chef
+            </Heading>
+            <HStack>
+              <Link href="/login">
+                <Button
+                  color="#d87e79"
+                  size="md"
+                  height="70px"
+                  width="600px"
+                  type="submit"
+                  onClick={() => {}}
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button
+                  color="#d87e79"
+                  size="md"
+                  height="70px"
+                  width="600px"
+                  type="submit"
+                  onClick={() => {}}
+                >
+                  Sign Up
+                </Button>
+              </Link>
+            </HStack>
+          </VStack>
+        </Flex>
+      </Center>
+    </Box>
   );
 }
